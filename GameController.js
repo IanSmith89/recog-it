d4a90a402d4c841e2692df813db4f964a214b723
Ian Smith <iansmith1026@gmail.com>
2016-01-24 01:53:52 -0700

    updated GameController to handle level and screen states, generates tiles based and fills grid

--- GameController.js
+++ GameController.js
@@ -5,5 +5,30 @@ angular.module('recogApp')
 
 function GameController() {
   var vm = this;
-  vm.title = 'recog.io';
+  vm.home_screen = true;
+  vm.game_screen = false;
+  vm.recall_screen = false;
+  vm.title = 'memor.io';
+  vm.level = 1;
+  vm.current = 1;
+  vm.target = (vm.level + 1) * (vm.level + 1);
+  vm.timer = 0;
+  vm.tileList = [];
+  vm.generateTiles = generateTiles;
+
+  function generateTiles() {
+    var randArr = [];
+    for (var i = 1; i <= vm.target; i++) {
+      randArr[i] = i;
+    }
+    randArr.sort(function() {
+      return Math.random() - 0.5;
+    });
+    randArr.map(function(num) {
+      var tile = {};
+      tile.value = num;
+      tile.flipped = false;
+      vm.tileList.push(tile);
+    });
+  }
 }
